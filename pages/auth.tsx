06b2bd08479b980357b9d861137b57befaf13a5f
leodder <a0919989772@gmail.com>
2024-02-22 19:27:41 +0800

    2024.2.22

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,19 @@
 //hooks
 import { useState,useCallback } from "react";
+// import { useRouter } from "next/router";
 //components
-import Input from "../components/input"
+import Input from "../components/input";
+//axios
+import axios from 'axios';
+//signin
+import {signIn} from 'next-auth/react';
+//icons
+import {FcGoogle} from 'react-icons/fc';
+import {FaGithub} from 'react-icons/fa'
 
 const Auth = () =>{
+    // const router = useRouter();
+
     const [email,setEmail] = useState("");
     const [name, setName] = useState("");
     const [password,setPassword] = useState("");
@@ -12,7 +22,38 @@ const Auth = () =>{
 
     const toggleVariant = useCallback(()=>{
         setVariant((currentVariant)=>currentVariant === 'login' ? 'register' : 'login')
-    },[])
+    },[]);
+
+    //login
+    const login = useCallback(async()=>{
+        try{
+            await signIn('credentials',{
+                email,
+                password,
+                // redirect:false,
+                callbackUrl:'/profiles'
+            });
+
+        }catch(error){
+            console.log(error)
+        }
+    },[email,password])
+
+    //register
+    const register = useCallback(async()=>{
+        try{
+            await axios.post('/api/register',{
+                email,
+                name,
+                password
+            });
+
+            login();
+        }catch(error){
+            console.log(error)
+        }
+    },[email, name, password,login])
+
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -49,9 +90,17 @@ const Auth = () =>{
                             value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div onClick={()=> signIn('google',{callbackUrl:'/profiles'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FcGoogle size={30}/>
+                                </div>
+                                <div onClick={()=> signIn('github',{callbackUrl:'/profiles'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FaGithub size={30}/>
+                                </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix' : 'Already have an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,49 @@
+//小bug:當我自己在測試時要注意github跟google的email是同一個，但因為是抓email有沒有重複的關係，所以假如當曾經用
+//github登入後，google的登入就會有問題，反之亦然
+
+import {signOut, getSession} from 'next-auth/react';
+//hooks
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useMovieList from '@/hooks/useMovieList';
+//protect the home route
+import { NextPageContext } from 'next';
+//components
+import Navbar from '@/components/navbar';
+import Billboard from '@/components/billBoard';
+import MovieList from '@/components/movieList';
+
+
+export async function getServerSideProps(context:NextPageContext) {
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props:{}
+  }
+};
 
 export default function Home() {
+  // const {data: user} =  useCurrentUser();
+  const { data:movies = []} = useMovieList();
+
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    {/* <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+    <p className='text-white'>Logged in as : {user?.name}</p>
+    <p className='text-white'>Logged in as : {user?.email}</p>
+    <button className='h-10 w-full bg-white' onClick={()=>signOut()}>Signout</button> */}
+    <Navbar />
+    <Billboard />
+    <div className='pb-40'>
+      <MovieList data={movies} title="Trending now" />
+    </div>
     </>
   );
 }
