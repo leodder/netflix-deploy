7734b54b49c8e9258109b762697d965158c85f84
leodder <a0919989772@gmail.com>
2024-03-18 15:48:02 +0800

    0318-14

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -3,22 +3,22 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-// export async function getServerSideProps(context: NextPageContext){
-//     const session = await getSession(context);
+export async function getServerSideProps(context: NextPageContext){
+    const session = await getSession(context);
 
-//     if(!session){
-//         return {
-//             redirect:{
-//                 destination:'/auth',
-//                 permanent:false,
-//             }
-//         }
-//     }
+    if(!session){
+        return {
+            redirect:{
+                destination:'/auth',
+                permanent:false,
+            }
+        }
+    }
 
-//     return {
-//         props:{}
-//     }
-// }
+    return {
+        props:{}
+    }
+}
 
 const Profiles = () => {
 
